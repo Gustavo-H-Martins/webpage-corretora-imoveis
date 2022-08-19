e2b2c48c39e962d706874550b1b9c0496c2aa661
Gustavo Henrique Lopes Martins <70769813+Gustavo-H-Martins@users.noreply.github.com>
2022-08-19 20:40:30 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Corretora_imovel_gustavo_martins.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<Corretora_imovel_gustavo_martinsContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("Corretora_imovel_gustavo_martinsContext") ?? throw new InvalidOperationException("Connection string 'Corretora_imovel_gustavo_martinsContext' not found.")));
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
